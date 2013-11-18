841b869e1d92a32d28322f6206cec2ec0bb4786f
Samy Pessé <samypesse@gmail.com>
2013-11-18 16:27:14 +0100

    Add realtime sync

--- views/markdown.js
+++ views/markdown.js
@@ -9,6 +9,7 @@ define([
     var hr = codebox.require("hr/hr");
     var Dialogs = codebox.require("utils/dialogs");
     var FilesBaseView = codebox.require("views/files/base");
+    var FileSync = codebox.require("utils/filesync");
 
     var FileMarkdownView = FilesBaseView.extend({
         className: "addon-files-markdownpreviewer",
@@ -19,17 +20,34 @@ define([
         // Constructor
         initialize: function() {
             FileMarkdownView.__super__.initialize.apply(this, arguments);
+
+            // base content
+            this.content = "";
+
+            // Showdown client
             this.converter = new Showdown.converter({ extensions: ['github', 'table'] });
+
+            // Create sync
+            this.sync = new FileSync();
+            this.sync.on("content", _.debounce(function(content, oldcontent, patches) {
+                this.updateContent(content);
+            }, 800), this);
+
+            // Active sync
+            this.sync.setFile(this.model);
+
             return this;
+        },
+
+        // Update markdown content
+        updateContent: function(content) {
+            this.content = content || this.content;
+            this.$(".markdown-body").html(this.converter.makeHtml(this.content));
         },
 
         // Finish Rendering
         finish: function() {
-            var that = this;
-            this.model.getCache().then(function(content) {
-                that.$(".markdown-body").html(that.converter.makeHtml(content));
-            });
-            
+            this.updateContent();
             return FileMarkdownView.__super__.finish.apply(this, arguments);
         },
     });
